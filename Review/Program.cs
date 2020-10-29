f7d9ae081cefc3bbfbac9ba87b6451224f877a74
Remi Douah <redmidouah@gmail.com>
2020-10-29 10:47:19 -0500

    Loop Practice

---
+++ Review/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            var myNumber = 0;
+
+            do
+            {
+                myNumber++;
+                numbers.Add(myNumber);
+
+            } while (myNumber < 25);
+
+            while (myNumber < 50)
+            {
+                myNumber++;
+                numbers.Add(myNumber);
+            }
+            
+            Console.WriteLine("Increase:");
+
+            
+            
+            foreach(var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Decrease:");
+
+            for (int i = 24; i <= numbers.Count && i >= 0; i--)
+            {
+                Console.WriteLine($" {numbers[i]}");
+            }
+               
+        }
+    }
+}
